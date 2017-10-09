d25a7650004b3d0cde4b1071e673de75c6b87fbd
pedrogms82 <pedrogms82@gmail.com>
2017-10-09 12:12:56 +0200

    actualizando rutas

--- storage/redis_test.go
+++ storage/redis_test.go
@@ -17,7 +17,7 @@ var r *RedisClient
 const prefix = "test"
 
 func TestMain(m *testing.M) {
-	r = NewRedisClient(&Config{Endpoint: "35.187.240.179:6379",Database: 10}, prefix,1000000, "MUSIC")
+	r = NewRedisClient(&Config{Endpoint: "127.0.0.1:6379",Database: 10}, prefix,1000000, "MUSIC")
 	reset()
 	c := m.Run()
 	reset()
@@ -612,5 +612,3 @@ func reset() {
 		r.client.Del(k)
 	}
 }
-
-
